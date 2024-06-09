469acdd2fac25710432a721a2141a192cf2d0f8e
st762590 <shivamtiwarirst@gmail.com>
2024-06-09 23:17:17 +0530

    update

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+/*==================================== toggle  ===================*/
+
+let nemuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classlist.toggle('fa-x');
+  navbar.classList.toggle('active')
+}
