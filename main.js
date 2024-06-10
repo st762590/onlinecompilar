092379e6799c017670184e4b91b153855984a9f6
st762590 <shivamtiwarirst@gmail.com>
2024-06-10 15:55:58 +0530

    update

--- main.js
+++ main.js
@@ -21,9 +21,9 @@ window.onscroll = () => {
     let id = sec.getAttribute('id');
     
     if(top >= offset && top < offset + height) {
-      navLinks.forEach.apply(links => {
+      navLinks.forEach(links => {
         links.classList.remove('active');
-        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        document.querySelector('header nav a[href*='+id + ']').classList.add('active');
       });
     };
   });
