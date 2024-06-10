38f482508310e24d4be1fc07601ddc6ebbcc8611
st762590 <shivamtiwarirst@gmail.com>
2024-06-10 08:13:50 +0530

    update

--- main.js
+++ main.js
@@ -7,3 +7,33 @@ menuIcon.onclick = () => {
   menuIcon.classlist.toggle('fa-x');
   navbar.classList.toggle('active')
 }
+
+/*==================================== scroll section active link  ===================*/
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+    
+    if(top >= offset && top < offset + height) {
+      navLinks.forEach.apply(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+    };
+  });
+  
+  /*==================================== sticky navbar  ===================*/
+
+  let header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 100);
+  
+  /*==================================== remove toggle icon and navbar  ===================*/
+  menuIcon.classList.remove('fa-x');
+  navbar.classList.remove('active');
+};
